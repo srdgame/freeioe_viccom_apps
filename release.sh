71978834747636009affe0971b653f48224e40b4
Dirk Chang <dirk@kooiot.com>
2023-11-09 15:20:21 +0800

    added yanli byc

--- release.sh
+++ release.sh
@@ -9,3 +9,5 @@ RELEASE_APP Net_info
 RELEASE_APP S7-200smart
 RELEASE_APP Port_tunnel
 RELEASE_APP toThingsBoard
+RELEASE_APP _Customer_Projects/wicsun.com/yanli_delta_byc
+RELEASE_APP _Customer_Projects/wicsun.com/yanli_delta_ccd
